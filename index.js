0dffa8319a875b8ea282f1b7147b677ec43c95b2
Stian Hanssen <stiahans75@gmail.com>
2020-06-24 14:48:11 +0200

    index.html index.js split

---
+++ index.js
@@ -0,0 +1,13 @@
+miro.onReady(() => {
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: 'Update Week Canvas',
+        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        onClick: () => {
+          alert('Updated!')
+        }
+      }
+    }
+  })
+})
