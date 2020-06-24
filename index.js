f179332521cf82dd3e7afeb7e31bb093cc2b967d
Stian Hanssen <stiahans75@gmail.com>
2020-06-24 14:49:10 +0200

    Check update frequency

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ miro.onReady(() => {
         title: 'Update Week Canvas',
         svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         onClick: () => {
-          alert('Updated!')
+          alert('Update...')
         }
       }
     }
