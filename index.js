bab682c760348f239b8159e726224163dfe98507
Stian Hanssen <stiahans75@gmail.com>
2020-06-24 15:33:53 +0200

    Update button properties

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ miro.onReady(() => {
     extensionPoints: {
       toolbar: {
         title: 'Update Week Canvas',
-        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        toolbarSvgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        librarySvgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         onClick: () => {
           alert('Update...')
         }
