e0b6c3a0a46785065302e9d275e93ce19c31912e
Stian Hanssen <stiahans75@gmail.com>
2020-06-24 14:53:40 +0200

    Update button and crewate button

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ miro.onReady(() => {
         onClick: () => {
           alert('Update...')
         }
+      },
+      bottomBar: {
+        title: 'Create Week Canvas',
+        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        onClick: () => {
+          alert('Creating...')
+        }
       }
     }
   })
