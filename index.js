e7a9fcfaae779e238dec92af368b1d4118e50dcb
Stian Hanssen <stiahans75@gmail.com>
2020-06-25 14:29:31 +0200

    Fetching week canvas and checking intersecting objects

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function checkForCanvasWidgets(widgets){
   updateCanvas(canvasWidgets, widgets);
 }
 
-function updateCanvas(canvasWidgets){
+function updateCanvas(canvasWidgets, widgets){
   for (let i = 0; i < canvasWidgets.length; i++){
     let cw = canvasWidgets[i];
     console.log('CW ' + cw.plainText + ' - ' + cw.type);
